1b1053a3e9609de7005722a1556ad5e4f564438c
nBiantoro <n.biantoro@simptel.com>
2023-05-30 11:52:59 +0200

    added js function to filter via phase tags

---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const tags = document.querySelectorAll('.tag');
+    const items = document.querySelectorAll('.item');
+  
+    tags.forEach(tag => {
+      tag.addEventListener('click', function() {
+        const filter = tag.getAttribute('data-filter');
+  
+        // Reset background color for all tags
+        tags.forEach(t => {
+          t.style.backgroundColor = '';
+          t.classList.remove('active');
+        });
+  
+        // Set background color for the clicked tag
+        tag.style.backgroundColor = 'purple';
+        tag.classList.add('active');
+  
+        items.forEach(item => {
+          item.style.display = 'none';
+  
+          if (filter === 'all' || item.classList.contains(filter)) {
+            item.style.display = 'block';
+          }
+        });
+      });
+    });
+  });
+  
+  
